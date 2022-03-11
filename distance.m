5b746c9268f583294c10fd8f5a2e9e647ac8e020
Uğur Yüzgeç <uyuzgec@gmail.com>
2022-03-11 11:51:22 +0300

    Update distance.m

--- distance.m
+++ distance.m
@@ -1,24 +1,3 @@
-%___________________________________________________________________%
-%  Dragonfly Algorithm (DA) source codes demo version 1.0           %
-%                                                                   %
-%  Developed in MATLAB R2011b(7.13)                                 %
-%                                                                   %
-%  Author and programmer: Seyedali Mirjalili                        %
-%                                                                   %
-%         e-Mail: ali.mirjalili@gmail.com                           %
-%                 seyedali.mirjalili@griffithuni.edu.au             %
-%                                                                   %
-%       Homepage: http://www.alimirjalili.com                       %
-%                                                                   %
-%   Main paper:                                                     %
-%                                                                   %
-%   S. Mirjalili, Dragonfly algorithm: a new meta-heuristic         %
-%   optimization technique for solving single-objective, discrete,  %
-%    and multi-objective problems, Neural Computing and Applications% 
-%   DOI: http://dx.doi.org/10.1007/s00521-015-1920-1                %
-%                                                                   %
-%___________________________________________________________________%
-
 % This function calculate the Euclidean distance 
 
 function o = distance(a,b)
